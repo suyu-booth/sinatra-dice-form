23c044f4b7b494e0f861384697fd98a8e32ee6d9
suyu-booth <suyu@chicagobooth.edu>
2024-10-26 02:58:34 +0000

    1

--- app.rb
+++ app.rb
@@ -2,8 +2,20 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/process_roll?dice=:number_of_dice&sides=:how_many_sides") do
+  @dice = params.fetch("number_of_dice").to_i
+  @sides = params.fetch("how_many_sides").to_i
+
+  @rolls = []
+
+  @dice.times do
+    die = rand(1..@sides)
+
+    @rolls.push(die)
+  end
+
+  erb(:roll)
 end
